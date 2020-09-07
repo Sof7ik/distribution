12de3cd1031dd4e604832e545f095f6d00f40789
Leonid <bychkov.l47@mail.ru>
2020-09-07 23:19:59 +0300

    clear main.js

--- dev/scripts/main.js
+++
@@ -1,6 +0,0 @@
-function logout () {
-    document.cookie = `user = ''; path=/; expires = ${new Date().getHours() - 24}`;
-    console.log('Logout');
-}
-
-// document.getElementById('logout-button').addEventListener('click', logout);
