71ba5e3e671df4d44ba2a9fd29dd10633f0a8bfc
Alexander <mrreads@yandex.com>
2020-09-26 15:48:52 +0300

    stylize export table

--- php/createExelFile.php
+++ php/createExelFile.php
@@ -10,10 +10,12 @@ use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 $spreadsheet = new Spreadsheet();
 $sheet = $spreadsheet->getActiveSheet();
 
-$sheet->setCellValue('A1', 'ФИО');
-$sheet->setCellValue('B1', 'Предмет');
-$sheet->setCellValue('C1', 'Группа');
-$sheet->setCellValue('D1', 'Часы');
+$sheet->setCellValue('A1', 'ФИО')->getStyle('A1')->getAlignment()->setHorizontal('right');
+$sheet->setCellValue('B1', 'Предмет')->getStyle('B1')->getAlignment()->setHorizontal('left');
+$sheet->setCellValue('C1', 'Группа')->getStyle('C1')->getAlignment()->setHorizontal('center');
+$sheet->setCellValue('D1', 'Часы')->getStyle('D1')->getAlignment()->setHorizontal('center');
+
+$spreadsheet->getActiveSheet()->getStyle('A1:D1')->getFont()->setBold(true);
 
 $cellValue = 2;
 foreach($total as $tchr)
@@ -30,16 +32,16 @@ foreach($total as $tchr)
         if ($nameIsOut)
         {
             $sheet->setCellValue('A' . $cellValue, '');
-            $sheet->setCellValue('B' . $cellValue, $item);
-            $sheet->setCellValue('C' . $cellValue, $group);
-            $sheet->setCellValue('D' . $cellValue, $hour);
+            $sheet->setCellValue('B' . $cellValue, $item)->getStyle('B' . $cellValue)->getAlignment()->setHorizontal('left');
+            $sheet->setCellValue('C' . $cellValue, $group)->getStyle('C' . $cellValue)->getAlignment()->setHorizontal('center');
+            $sheet->setCellValue('D' . $cellValue, $hour)->getStyle('D' . $cellValue)->getAlignment()->setHorizontal('center');
         }
         else
         {
-            $sheet->setCellValue('A' . $cellValue, $tchr['fio']);
-            $sheet->setCellValue('B' . $cellValue, $item);
-            $sheet->setCellValue('C' . $cellValue, $group);
-            $sheet->setCellValue('D' . $cellValue, $hour);
+            $sheet->setCellValue('A' . $cellValue, $tchr['fio'])->getStyle('A' . $cellValue)->getAlignment()->setHorizontal('right');
+            $sheet->setCellValue('B' . $cellValue, $item)->getStyle('B' . $cellValue)->getAlignment()->setHorizontal('left');
+            $sheet->setCellValue('C' . $cellValue, $group)->getStyle('C' . $cellValue)->getAlignment()->setHorizontal('center');
+            $sheet->setCellValue('D' . $cellValue, $hour)->getStyle('D' . $cellValue)->getAlignment()->setHorizontal('center');
             $nameIsOut = true;
         }
 
@@ -47,10 +49,27 @@ foreach($total as $tchr)
     }
     $sheet->setCellValue('A' . $cellValue, '');
     $sheet->setCellValue('B' . $cellValue, '');
-    $sheet->setCellValue('C' . $cellValue, 'Итого:');
-    $sheet->setCellValue('D' . $cellValue, $tchr['hours']);
+    $sheet->setCellValue('C' . $cellValue, 'Итого:')->getStyle('C' . $cellValue)->getAlignment()->setHorizontal('right');
+    $sheet->setCellValue('D' . $cellValue, $tchr['hours'])->getStyle('D' . $cellValue)->getAlignment()->setHorizontal('center');
+
+    $spreadsheet->getActiveSheet()->getStyle('C' . $cellValue)->getFont()->setBold(true);
+    $spreadsheet->getActiveSheet()->getStyle('D' . $cellValue)->getFont()->setBold(true);
 
     $cellValue++;
+
+
+    $sheet->setCellValue('A' . $cellValue, '');
+    $sheet->setCellValue('B' . $cellValue, '');
+    $sheet->setCellValue('C' . $cellValue, '');
+    $sheet->setCellValue('D' . $cellValue, '');
+    
+    $cellValue++;
+}
+
+$sheet = $spreadsheet->getActiveSheet();
+foreach ($sheet->getColumnIterator() as $column) 
+{
+    $sheet->getColumnDimension($column->getColumnIndex())->setAutoSize(true);
 }
 
 $fileName = './../generated/Таблица_распределения_' . date("m.d.y") . '.xlsx';
