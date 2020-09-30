c1f9089aad5aeef9f7e23c09183ea86a2db86e13
406 <406@mail.ru>
2020-09-30 13:28:33 +0300

    fix navigation

--- pages/admin/includes/nav.php
+++ pages/admin/includes/nav.php
@@ -5,14 +5,22 @@
     </a>
 
     <nav>
-        <a href="./add_group.php">Добавить группу</a>
-        <a href="./add_profile.php">Добавить профиль, в котором ещё предметы там...</a>
         <a href="./add_specialization.php">Добавить специальность</a>
+        <a href="./add_group.php">Добавить группу</a>
+        <a href="./add_profile.php">Добавить профиль</a> 
         <a href="./add_subject.php">Добавить предмет</a>
-        <a href="./allgroups.php">Распределить предметы</a>
-        <a href="./all_specs.php">Распределить специальности</a>
         <a href="./add_teacher.php">Добавить учителя</a>
-        <a href="./create_table.php">Софрмировать таблицу преподавательской нагрузки</a>
+
+        <br>
+        <br>
+
+        <a href="./all_specs.php">Распределить предметы по специальностям</a>
+        <a href="./allgroups.php">Распределить предметы по группам</a>
+
+        <br>
+        <br>
+        
+        <a href="./create_table.php">Сформировать таблицу преподавательской нагрузки</a>
 
     </nav>
 
